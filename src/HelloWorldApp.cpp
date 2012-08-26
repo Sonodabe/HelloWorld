e05391734631699744b47aa1f0b10068125012df
Brandon Sonoda <sonodabe@muohio.edu>
2012-08-26 18:15:24 -0400

    Added Variables and color changing
    This is the first time I used variables in c++, I played around with
    changing the variables.
    
    I also used conditionals in this one, and they seem to work out just
    like in java…
    
    The overall effect of these changes are a screen that shifts between
    many different colors.

--- src/HelloWorldApp.cpp
+++ src/HelloWorldApp.cpp
@@ -11,10 +11,15 @@ class HelloWorldApp : public AppBasic {
 	void mouseDown( MouseEvent event );	
 	void update();
 	void draw();
+private:
+    float red, green, blue;
 };
 
 void HelloWorldApp::setup()
 {
+    red = 0;
+    green = .5;
+    blue = 1.0;
 }
 
 void HelloWorldApp::mouseDown( MouseEvent event )
@@ -23,12 +28,21 @@ void HelloWorldApp::mouseDown( MouseEvent event )
 
 void HelloWorldApp::update()
 {
+    red+=.01;
+    green+=.025;
+    blue+=.003;
+    if(red > 1)
+        red = 0;
+    if(green > 1)
+        green = 0;
+    if(blue  > 1)
+       blue = 0;
 }
 
 void HelloWorldApp::draw()
 {
 	// clear out the window with black
-	gl::clear( Color( 0, 0, 0 ) ); 
+	gl::clear( Color( red, green, blue ) ); 
 }
 
 CINDER_APP_BASIC( HelloWorldApp, RendererGl )
